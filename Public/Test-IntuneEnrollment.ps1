d0a1ad1256e8b9e40597eb1b23ac3594f77e51d1
Jonathan W. Smith <81338645+SmithingJon@users.noreply.github.com>
2022-07-07 16:56:16 -0500

    Re-wrote logic to remove dependencies on Get-ManagedDeviceGroupMembership

--- Public/Test-IntuneEnrollment.ps1
+++ Public/Test-IntuneEnrollment.ps1
@@ -1,7 +1,12 @@
 function Test-IntuneEnrollment{
-    Test-MSGraph
+    $ProgressPreference = 'SilentlyContinue'
+    # Get a user token for MS Graph
+    $GraphToken = Connect-MSGraph -PassThru
     $serial = (Get-WmiObject -Class "WIN32_BIOS" -Property serialNumber).serialNumber
-    $device = Get-DeviceManagement_ManagedDevices | Where-Object serialNumber -Contains "$serial"
+    $SerialURL = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?`$filter=serialNumber eq '$serial'&`$select=azureADDeviceId"
+    $headers = @{'Authorization'="Bearer " + $GraphToken}
+    $D_Response = Invoke-WebRequest -Uri $SerialURL -Method GET -Headers $Headers -UseBasicParsing
+    $test = ($D_Response.Content | ConvertFrom-Json).Value.azureADDeviceId
     if ($device) {
         $true
     }

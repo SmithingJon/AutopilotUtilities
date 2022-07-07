8200ce625d86f4b028c4e7bfb4e9c8c67dd95d84
Jonathan W. Smith <81338645+SmithingJon@users.noreply.github.com>
2022-07-07 18:49:10 -0500

    Removed dependency from Get-WindowsAutopilotInfo

--- Public/Get-AutopilotRecord.ps1
+++ Public/Get-AutopilotRecord.ps1
@@ -1,9 +1,12 @@
 function Get-AutopilotRecord {
     [CmdletBinding()]
     param (
+        [Parameter()]
+        [string]
+        $serial = (Get-WmiObject -Class "WIN32_BIOS" -Property serialNumber).serialNumber
     )
     Test-MSGraph
-    $serial = (Get-WmiObject -Class "WIN32_BIOS" -Property serialNumber).serialNumber
-    $autopilotrecord = Get-AutopilotDevice | Where-Object serialNumber -eq "$serial"
+    $URI = "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeviceIdentities?`$filter=contains(serialNumber,'$($serial)')"
+    $autopilotrecord = (Invoke-MSGraphRequest -Url $uri -HttpMethod GET -ErrorAction Stop).Value
     $autopilotrecord
 }
